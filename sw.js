b40e538e4b75a0ea900f4588cab885f41cabb24b
juul1703 <juliette-porienski@hotmail.com>
2026-09-10 00:05:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Simpele offline-cache. Verhoog VERSIE na elke wijziging. */
-var VERSIE = 'ssms-v24';
+var VERSIE = 'ssms-v25';
 var BESTANDEN = ['./', './index.html', './les.html', './vak.html', './styles.css', './app.js',
   './rooster.js', './les.js', './lesextra.js', './vak.js', './lesblokken.js', './lesstof.js',
   './ssms-inhoud.js', './manifest.webmanifest'];
